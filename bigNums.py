c9ad21b77f3585f0c3fb41957708df8bdf1b205f
6kts0 <fabakouyate06@gmail.com>
2025-12-21 00:31:00 -0800

    Max printable expontial numbers, and infinite limits

--- bigNums.py
+++ bigNums.py
@@ -1,3 +1,17 @@
-exp_num = 1e+309
-print(a is bigNums.exp_num, end='')
+# Highest printable exponential number
+exp_max = 1e+308
+print(exp_max)
+print(type(exp_max))
+print('-----------')
 
+# Regular printable exponential number
+exp_num = 1e+105
+print(exp_num)
+print(type(exp_num))
+print('-----------')
+
+# Exponential turn infinite number
+exp_inf = 1e+309
+print(exp_inf)
+print(type(exp_inf))
+print('-----------')
