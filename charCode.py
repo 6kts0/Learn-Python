bfa7e2784e5b919262c56359996f94a327008003
6kts0 <fabakouyate06@gmail.com>
2026-02-26 01:32:34 -0800

    ord() - Return unicode of a char - Adding characters

--- charCode.py
+++ charCode.py
@@ -1,11 +1,13 @@
 S = 'W'
 P = 'python'
 
-# ord returns the binary code of a char in memory
+# ord() returns the unicode of any char
 print(ord('O'))
 
-# You can also do math with ord
-print(ord(S) + 10) 
+# You can also do math - e.g. ord() + int
+print(f"{ord(S) + 10}")
+
+print(f"Unicode a + Unicode b = {ord('a') + ord('b')}")
 
 # Looping through a string to return each binary digits
 def main():
