6e2579add1a53215b7f0c1f05a1680497924b47f
6kts0 <fabakouyate06@gmail.com>
2026-03-03 19:50:51 -0800

    Refine class and function call structure - Populated SUV class and added print formatting

--- Objects/Dealership/manifold.py
+++ Objects/Dealership/manifold.py
@@ -1,15 +1,18 @@
+from currencies import Currency
+
+currency = Currency('USD')
+
 # Ford Inventory
 class vehicle_class():
-    def __init__(self, name, classID):
-        self.name = name
+    def __init__(self, className, classID):
+        self.className = className
         self.classID = classID
 
-    def CLS(self):
-        print("Vehicle Class:", self.name, self.classID)
 
-"""
-Vehicle class IDs
-"""
+    def CLS(self):
+        print('|', self.className, '|', self.classID, '|')
+
+# VEHICLE IDs
 # coupe: 11
 # sport: 13
 # suv: 17
@@ -21,47 +24,57 @@ sport = vehicle_class("Sport", 13)
 suv = vehicle_class("SUV", 17)
 truck = vehicle_class("Truck", 19)
 offroad = vehicle_class("Off-Road", 29)
-track = vehicle_class("track", 79)
-
-print("Classes: | Coupe -- Sport -- SUV -- Truck -- Off-Road -- Track |")
-view_class = input("(ADMIN) View vehicle class IDs: ").lower().strip()
-if view_class == 'coupe':
-    coupe.CLS()
-elif view_class == 'sport':
-    sport.CLS()
-elif view_class == 'suv':
-    suv.CLS()
-elif view_class == 'truck':
-    truck.CLS()
-elif view_class == 'offroad' or view_class == 'off-road':
-    offroad.CLS()
-elif view_class == 'track':
-    track.CLS()
+track = vehicle_class("Track", 79)
 
 class styleSUV():
-    def __init__(self, name, year, msrp, engine, gearbox, power, vin):
+    def __init__(self, name, year, msrp, engine, gearbox, power, className=suv):
         self.name = name
         self.year = year
         self.msrp = msrp
         self.engine = engine
         self.gearbox = gearbox
         self.power = power
-        self.vin = vin
+        self.className = className
 
     def SUV(self):
-        print("Vehicle summary:", self.name, self.year, self.msrp, self.engine, self.gearbox, self.power, self.vin)
+        print("| Model | Year | MSRP | Engine | Gearbox | Power (WHP) |")
+        print('|', self.name, '|', self.year, '|', currency.get_money_with_currency_format(self.msrp), '|', self.engine, '|', self.gearbox, '|', self.power, )
+        print('-' * 80)
 
-fordKuga = styleSUV("Ford Kuga", 2024, 32000, "1.5L EcoBoost", "6-Speed Manual", "150HP", 15787)
+fordKuga = styleSUV('Ford Kuga', 2024, '32,000', '1.5L EcoBoost', '6-Speed Manual', '150', suv)
+fordExpd = styleSUV('Ford Expedition', 2025, '62,400', '3.5L EcoBoost V6', '10-Speed Automatic Transmission', '406', suv)
+fordMache =  styleSUV('Ford Mustang MACH-E', 2023, '25,985', '98.8 Kw Electric', 'Single-Speed/Reduction Drive', 465, suv)
 
-fordKuga.SUV()
+def viewSuv_inv():
+    suvCat = input("Would you like to see our inventory (Yes/No): ").lower().strip()
+    if suvCat == 'yes':
+        print('=' * 80)
+        print("SUV - IN STOCK")
+        print('=' * 80)
+        fordKuga.SUV()
+        fordExpd.SUV()
+        fordMache.SUV()
+    else:
+        exit
 
-
-
-
-
-
-
-
-
-
-
+print("-- VEHICLE CLASS TYPES --")
+print('=' * 80)
+print("| Coupe | Sport | SUV | Truck | Off-Road | Track |")
+print('=' * 80)
+view_class = input("Show vehicle class info: ").lower().strip()
+if view_class == 'coupe':
+    coupe.CLS()
+elif view_class == 'sport':
+    sport.CLS()
+elif view_class == 'suv':
+    print('=' * 80)
+    print("| Class Name | Class ID")
+    suv.CLS()
+    print('=' * 80)
+    viewSuv_inv()
+elif view_class == 'truck':
+    truck.CLS()
+elif view_class == 'offroad' or view_class == 'off-road':
+    offroad.CLS()
+elif view_class == 'track':
+    track.CLS()
